fd9bfa651904851ae7d84c96e3dbd14c9ef94d40
Hoangvu75 <uchihavuhuyhoang@gmail.com>
2022-11-26 10:23:56 +0700

    update

--- src/model/account.ts
+++ src/model/account.ts
@@ -3,7 +3,7 @@ import { Schema, model } from 'mongoose';
 
 const Account = new Schema({
     username: { type: String, required: true, unique: true, maxLength: 30, minLength: 8 },
-    password: { type: String, required: true },
+    password: { type: String, required: true, maxLength: 30, minLength: 8 },
 })
 
 export default model("account", Account);
