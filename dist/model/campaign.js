3b51497d5a6de6d3940d21fbe493054542f286e4
Hoangvu75 <uchihavuhuyhoang@gmail.com>
2022-11-26 15:25:45 +0700

    add

--- dist/model/campaign.js
+++ dist/model/campaign.js
@@ -5,6 +5,7 @@ const Campaign = new mongoose_1.Schema({
     title: { type: String, required: true, minLength: 8 },
     description: { type: String, required: true, minLength: 8 },
     start_time: { type: Number, required: true },
+    image: { type: String },
     followers: { type: Array }
 });
 exports.default = (0, mongoose_1.model)("campaign", Campaign);
--- src/model/campaign.ts
+++ src/model/campaign.ts
@@ -5,6 +5,7 @@ const Campaign = new Schema({
     title: { type: String, required: true, minLength: 8 },
     description: { type: String, required: true, minLength: 8 },
     start_time: { type: Number, required: true },
+    image: { type: String },
     followers: { type: Array }
 })
 
